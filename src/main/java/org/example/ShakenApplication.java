ffa6931cd2b223c827d6dc75b5d02422ce581278
Stephen Kaczmarowski <stephenkaczmarowski@Stephens-MacBook-Pro.local>
2023-06-15 16:54:06 -0700

    started creating console menu for testing

--- src/main/java/org/example/ShakenApplication.java
+++ src/main/java/org/example/ShakenApplication.java
@@ -4,19 +4,43 @@ import java.util.Scanner;
 
 public class ShakenApplication {
 
+    private static final String MAIN_MENU_WELCOME = "Welcome to Shaken!";
+    private static final String MAIN_MENU_OPTION_SEARCH = "Search Cocktail";
+    private static final String MAIN_MENU_OPTION_INGREDIENTS = "Find A Drink With Ingredients You Have";
+    private static final String MAIN_MENU_OPTION_CREATE = "Create New Cocktail";
+    private static final String MAIN_MENU_OPTION_EXIT = "Exit Application";
+    private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_SEARCH, MAIN_MENU_OPTION_INGREDIENTS, MAIN_MENU_OPTION_CREATE, MAIN_MENU_OPTION_EXIT};
+
+    private Menu menu;
     RecipeList recipeList = new RecipeList();
     Scanner userInput = new Scanner(System.in);
+
+    public ShakenApplication(Menu menu) {
+        this.menu = menu;
+    }
     public void run() {
 
-        System.out.println("Welcome to Shaken!");
-        System.out.println("Please search for a cocktail:");
-        String searchedCocktail = userInput.nextLine();
-        recipeList.searchRecipe(searchedCocktail);
+        while (true) {
+            String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
+
+
+            if (choice.equals(MAIN_MENU_OPTION_SEARCH)) {
+                recipeList.searchRecipe(userInput);
+            } else if (choice.equals(MAIN_MENU_OPTION_INGREDIENTS)) {
+                System.out.println("Searching by ingredients...");
+            } else if (choice.equals(MAIN_MENU_OPTION_CREATE)) {
+                System.out.println("Creating cocktail...");
+            } else if (choice.equals(MAIN_MENU_OPTION_EXIT)) {
+                System.out.println("Goodbye!");
+                break;
+            }
+
+        }
 
     }
     public static void main(String[] args) {
         Menu menu = new Menu(System.in, System.out);
-        ShakenApplication cli = new ShakenApplication();
+        ShakenApplication cli = new ShakenApplication(menu);
         cli.run();
 
     }
