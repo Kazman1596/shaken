47af279c679260d3cd9c3ba2f131e27943deba4b
Stephen Kaczmarowski <stephenkaczmarowski@Stephens-MacBook-Pro.local>
2023-06-15 07:03:49 -0700

    finished trimming data, created recipe object, and tested pulling data from recipeMap

---
+++ src/main/java/org/example/Recipe.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class Recipe {
+    private String title;
+    private String ingredients;
+    private String instructions;
+    private String glassWare;
+
+    public Recipe(String title, String ingredients, String instructions) {
+        this.title = title;
+        this.ingredients = ingredients;
+        this.instructions = instructions;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    //TODO: Make a method to determine glassware(for eventual picture)
+
+    public String setGlassware() {
+        return "";
+    }
+}
--- src/main/java/org/example/RecipeList.java
+++ src/main/java/org/example/RecipeList.java
@@ -1,8 +1,40 @@
 package org.example;
 
-public class RecipeList {
-    public void run() {
+import java.io.File;
+import java.util.HashMap;
+import java.util.Scanner;
 
+public class RecipeList {
+    File inputFile = new File("recipe-list.txt");
+    HashMap<String, Recipe> recipeMap = new HashMap<>();
+
+    public RecipeList() {
+
+        try(Scanner scanner = new Scanner(inputFile)) {
+            while (scanner.hasNextLine()) {
+                String recipeLine = scanner.nextLine();
+                String[] recipeArr = recipeLine.split(" \\| ");
+                String title = recipeArr[0];
+                Recipe recipe = new Recipe(recipeArr[0], recipeArr[1], recipeArr[2]);
+                recipeMap.put(title.toLowerCase(), recipe);
+            }
+        } catch(Exception ex) {
+            System.out.println("Problem fetching recipe");
+        }
     }
+
+    public void searchRecipe(String search) {
+        if (recipeMap.containsKey(search.toLowerCase())) {
+            Recipe recipe = recipeMap.get(search.toLowerCase());
+            System.out.println("This is the cocktail: " + recipe.getTitle());
+            System.out.println("INGREDIENTS:");
+            System.out.println(recipe.getIngredients());
+            System.out.println("INSTRUCTIONS:");
+            System.out.println(recipe.getInstructions());
+        } else {
+            System.out.println("Could not find your drink of choice");
+        }
+    }
+
 
 }
