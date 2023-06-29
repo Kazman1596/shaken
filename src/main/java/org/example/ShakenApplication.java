2e8dc0b4c1c2c44237206e987f293922ba92078a
Stephen Kaczmarowski <stephenkaczmarowski@Stephens-MacBook-Pro.local>
2023-06-29 12:48:08 -0700

    final cleanup of recipe.txt, created jdbcRecipeDao, createRecipe, getRecipeById, and mapResultsToRecipe

--- src/main/java/org/example/ShakenApplication.java
+++ src/main/java/org/example/ShakenApplication.java
@@ -1,9 +1,9 @@
 package org.example;
 
+import org.example.dao.JdbcRecipeDao;
+import org.example.model.Recipe;
 import org.example.model.RecipeList;
 
-import java.io.File;
-import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class ShakenApplication {
@@ -13,7 +13,7 @@ public class ShakenApplication {
     private static final String MAIN_MENU_OPTION_INGREDIENTS = "Find A Drink With Ingredients You Have";
     private static final String MAIN_MENU_OPTION_COCKTAIL = "Cocktail Database";
     private static final String MAIN_MENU_OPTION_EXIT = "Exit Application";
-    private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_SEARCH, MAIN_MENU_OPTION_INGREDIENTS, MAIN_MENU_OPTION_COCKTAIL, MAIN_MENU_OPTION_EXIT};
+    private static final String[] MAIN_MENU_OPTIONS = {MAIN_MENU_OPTION_SEARCH, MAIN_MENU_OPTION_INGREDIENTS, MAIN_MENU_OPTION_COCKTAIL, MAIN_MENU_OPTION_EXIT};
 
     private Menu menu;
     SearchResultsMenu searchResultsMenu = new SearchResultsMenu();
@@ -24,6 +24,7 @@ public class ShakenApplication {
     public ShakenApplication(Menu menu) {
         this.menu = menu;
     }
+
     public void run() {
 
         while (true) {
@@ -47,33 +48,13 @@ public class ShakenApplication {
     }
 
     public static void main(String[] args) {
+        RecipeList recipeList = new RecipeList();
+        JdbcRecipeDao jdbcRecipeDao = new JdbcRecipeDao();
         Menu menu = new Menu(System.in, System.out);
         ShakenApplication cli = new ShakenApplication(menu);
         cli.run();
-
-        //Clean up
-        File file = new File("recipe-list.txt");
-        File outputFile = new File("recipe-list2.txt");
-        try(Scanner scanner = new Scanner(file); PrintWriter writer = new PrintWriter(outputFile)) {
-            while (scanner.hasNextLine()) {
-                //Setting up scanner / writer
-                String line = scanner.nextLine();
-                String[] lineArray = line.split(" \\| ");
-                String title = lineArray[0];
-                String ingredients = lineArray[1];
-                String instructions = lineArray[2];
-
-                String newIngredients = " " + ingredients + " ";
-                //TODO: Splitting ingredients by comma
-
-                //Putting it back together
-                    System.out.println(newIngredients.trim());
-                //writer.println(title + " | " + newIngredients + " | " + instructions);
-            }
-        } catch (Exception ex) {
-            System.out.println("Trouble reading recipe");
+        for (Recipe recipe : recipeList.getRecipeList()) {
+            jdbcRecipeDao.createRecipe(recipe);
         }
-
     }
-
 }
