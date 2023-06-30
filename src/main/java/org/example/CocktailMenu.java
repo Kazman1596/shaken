435fa94a49149c04accaa9131eb887eb63e5919a
Stephen Kaczmarowski <stephenkaczmarowski@Stephens-MacBook-Pro.local>
2023-06-29 18:37:20 -0700

    Finished a bit more refactoring and started connecting cocktail database functions

--- src/main/java/org/example/CocktailMenu.java
+++ src/main/java/org/example/CocktailMenu.java
@@ -1,5 +1,11 @@
 package org.example;
 
+import org.example.dao.JdbcRecipeDao;
+import org.example.model.Recipe;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class CocktailMenu {
 
     private static final String CREATE_COCKTAIL = "Create cocktail";
@@ -9,19 +15,43 @@ public class CocktailMenu {
     private static final Object[] cocktailMenuOptions = {CREATE_COCKTAIL, UPDATE_COCKTAIL_MENU, DELETE_COCKTAIL_MENU, BACK_MENU};
 
     Menu menu = new Menu(System.in, System.out);
+    Scanner userInput = new Scanner(System.in);
+    SearchResultsMenu searchResultsMenu = new SearchResultsMenu();
+    JdbcRecipeDao jdbcRecipeDao = new JdbcRecipeDao();
 
     public void runCocktailDatabaseMenu() {
         while(true) {
             Object choice = menu.getChoiceFromOptions(cocktailMenuOptions);
             if(choice.equals(CREATE_COCKTAIL)) {
-                System.out.println("Unconnected Create Option");
+                Recipe newRecipe = createRecipePrompt(userInput);
+                jdbcRecipeDao.createRecipe(newRecipe);
             } else if (choice.equals(UPDATE_COCKTAIL_MENU)) {
-                System.out.println("Unconnected Update Option");
+                updateRecipePrompt(userInput);
             } else if (choice.equals(DELETE_COCKTAIL_MENU)) {
                 System.out.println("Unconnected Delete Option");
             } else if (choice.equals(BACK_MENU)) {
                 break;
             }
         }
+    }
+
+    public Recipe createRecipePrompt(Scanner in) {
+        System.out.println("Title:");
+        String title = in.nextLine().toUpperCase();
+        System.out.println("Ingredients (separated by a ' + ')");
+        String ingredients = in.nextLine();
+        System.out.println("Instructions");
+        String instructions = in.nextLine();
+
+        return new Recipe(title, ingredients, instructions, "Cocktail Glass", 1);
+    }
+
+    //TODO: UpdateRecipePrompt should return a recipe, and ultimately be put into the jdbcRecipeDao.updateRecipeById()
+    // might need to do a bit of rearranging in the SearchResultsMenu
+    public void updateRecipePrompt(Scanner in) {
+        System.out.println("Please search for a recipe you'd like to update");
+        String recipeSearch = in.nextLine();
+        List<Recipe> recipeResults = jdbcRecipeDao.getRecipesByTitle(recipeSearch, true);
+        searchResultsMenu.runSearchResultsMenu(recipeResults);
     }
 }
--- src/main/java/org/example/SearchResultsMenu.java
+++ src/main/java/org/example/SearchResultsMenu.java
@@ -2,7 +2,6 @@ package org.example;
 
 import org.example.dao.JdbcRecipeDao;
 import org.example.model.Recipe;
-import org.example.model.RecipeList;
 
 import java.util.List;
 
@@ -10,7 +9,6 @@ public class SearchResultsMenu {
     private static final String SEARCH_RESULT_MENU_BACK = "Go back";
     private static final Object[] searchResultMenuOptions = {SEARCH_RESULT_MENU_BACK};
     Menu menu = new Menu(System.in, System.out);
-    RecipeList recipeList = new RecipeList();
     JdbcRecipeDao jdbcRecipeDao = new JdbcRecipeDao();
 
     public void runSearchResultsMenu(List<Recipe> cocktailSearchResults) {
@@ -30,12 +28,5 @@ public class SearchResultsMenu {
                 break;
             }
         }
-    }
-
-    public Object[] createResultsOptions(List<Recipe> searchResults) {
-        List<Recipe> menuOptions = searchResults;
-//        menuOptions.add(SEARCH_RESULT_MENU_BACK);
-
-        return menuOptions.toArray();
     }
 }
