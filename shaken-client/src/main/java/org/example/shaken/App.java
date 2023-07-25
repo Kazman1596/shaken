b97ab334b44af1ec0ce8fbfe1e4ec4435184e5b7
Stephen Kaczmarowski <stephenkaczmarowski@Stephens-MacBook-Pro.local>
2023-07-25 11:10:47 -0700

    finished db and ingredient controllers

--- shaken-client/src/main/java/org/example/shaken/App.java
+++ shaken-client/src/main/java/org/example/shaken/App.java
@@ -1,18 +1,31 @@
 package org.example.shaken;
 
-import org.example.shaken.model.AuthenticatedUser;
-import org.example.shaken.model.Recipe;
-import org.example.shaken.model.UserCredentials;
+import org.example.shaken.model.*;
 import org.example.shaken.services.AuthenticationService;
 import org.example.shaken.services.ConsoleService;
+import org.example.shaken.services.IngredientService;
 import org.example.shaken.services.RecipeService;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
 public class App {
+
+    //TODO: Create getIngredientsByRecipeId()
+
+    //TODO: Create getRecipesByIngredient()
+
+    //TODO: Update createRecipe() to automatically add Ingredients and add to associative table
+
+    //TODO: Integration tests for all new DAO's
+
+    //TODO: createRecipe() should now accept currentUser.getId() as userId in Recipe class
+
+    //TODO: Hook up services and console application completely (add review, searchIngredients, etc)
+
+    //TODO: Go through and address comments, other TODO's, and anything else before starting Front-End
+
 
     private static final String MAIN_MENU_OPTION_SEARCH = "Search Cocktail";
     private static final String MAIN_MENU_OPTION_INGREDIENTS = "Find A Drink With Ingredients You Have";
@@ -28,8 +41,9 @@ public class App {
     RecipeService recipeService = new RecipeService();
     Scanner userInput = new Scanner(System.in);
     ConsoleService consoleService = new ConsoleService();
-
     AuthenticationService authenticationService = new AuthenticationService(API_BASE_URL);
+
+    IngredientService ingredientService = new IngredientService();
     private AuthenticatedUser currentUser;
 
     public static void main(String[] args) {
@@ -42,13 +56,6 @@ public class App {
         this.menu = menu;
     }
 
-    //TODO: login works without edge cases
-    // register does NOT, typing in a login that doesn't exist does NOT
-    // will pick up tomorrow
-    /*
-        Messages found in server-side, register needs firstName, lastName, and email as well
-        Forgot what the error said for login
-     */
     private void run() {
         consoleService.printGreeting();
         loginMenu();
@@ -145,16 +152,157 @@ public class App {
         return searchForIngredients(search);
     }
 
-    //TODO: Amazing, and the logic is there. However, this now only works if you say EXACTLY '1 1/2 ounce tequila'
-    // - Perhaps another data structure will help, OR now I'm thinking maybe another SQL table for ingredients so we can separate measurement from ingredient and create the map that way
     public List<Recipe> searchForIngredients(String userInput) {
-        HashMap<String, List<Recipe>> ingredientMap = recipeService.getIngredientMap();
         List<Recipe> results = new ArrayList<>();
         String[] userIngredientsArray = userInput.split(",");
-        for (String ingredient : userIngredientsArray) {
-            List<Recipe> recipes = ingredientMap.get(ingredient);
-            results.addAll(recipes);
-        }
         return results;
     }
+
+    private void disgustingMessToAddNonDuplicateIngredients() {
+        List<String> ingredientList = recipeService.getIngredientList();
+        List<Ingredient> ingredientDB = ingredientService.getIngredients();
+        List<Recipe> results = new ArrayList<>();
+        for (String ingredientString : ingredientList) {
+            System.out.println(ingredientString);
+            String[] ingredientArray = ingredientString.split(" \\+ ");
+            for (String ingredient : ingredientArray) {
+                System.out.println(ingredient);
+                int quantityIndex = 1;
+                String measurement = null;
+
+                //FINDING QUANTITY
+                for (int i = 1; i < ingredient.length(); i++) {
+
+                    if (!Character.isDigit(ingredient.charAt(i)) && !Character.isDigit(ingredient.charAt(i + 1)) && !Character.isDigit(ingredient.charAt(i + 2))) {
+                        quantityIndex = i;
+                        break;
+                    }
+                }
+
+                //FINDING MEASUREMENT
+                if (ingredient.contains("ounce")) {
+                    measurement = "ounce";
+                } else if (ingredient.contains("dash")) {
+                    measurement = "dash";
+                } else if (ingredient.contains("teaspoon")) {
+                    measurement = "teaspoon";
+                } else if (ingredient.contains("tablespoon")) {
+                    measurement = "tablespoon";
+                } else if (ingredient.contains("cup")) {
+                    measurement = "cup";
+                } else if (ingredient.contains("drop")) {
+                    measurement = "drop";
+                } else if (ingredient.contains("pinch")) {
+                    measurement = "pinch";
+                } else if (ingredient.contains("scoop")) {
+                    measurement = "scoop";
+                }
+
+
+                String ingredientTitle = "";
+                int measurementIndex = 0;
+                if (measurement == null) {
+                    ingredientTitle = ingredient.substring(quantityIndex + 1);
+                } else {
+                    String[] array = ingredient.split(" ");
+                    boolean pastMeasurement = false;
+                    for (int i = 1; i < array.length; i++) {
+                        if (pastMeasurement) {
+                            ingredientTitle += array[i] + " ";
+                        }
+                        if (array[i].contains(measurement)) {
+                            pastMeasurement = true;
+                        }
+                    }
+                }
+
+                ingredientDB = ingredientService.getIngredients();
+
+                System.out.println(ingredient.substring(0, quantityIndex)); //QUANTITY
+                System.out.println(measurement); //MEASUREMENT
+                System.out.println(ingredientTitle.trim()); //TITLE
+
+                boolean duplicate = false;
+
+                if(ingredient.contains("Crushed Ice")) {
+                    Ingredient newIngredient = new Ingredient("1/2", "cup", "crushed ice");
+                    if (ingredientDB.size() == 0) {
+                        ingredientService.createIngredient(newIngredient);
+                    } else {
+                        for (Ingredient ing : ingredientDB) {
+                            if (ing.getMeasurement() != null && newIngredient.getMeasurement() != null) {
+                                if (ing.getMeasurement().equals(newIngredient.getMeasurement()) && ing.getQuantity().equals(newIngredient.getQuantity()) && ing.getName().equals(newIngredient.getName())) {
+                                    duplicate = true;
+                                }
+                            } else {
+                                if (ing.getQuantity().equals(newIngredient.getQuantity()) && ing.getName().equals(newIngredient.getName())) {
+                                    duplicate = true;
+                                }
+                            }
+                        }
+                        if (!duplicate) {
+                            ingredientService.createIngredient(newIngredient);
+                        }
+                    }
+                } else {
+                    Ingredient newIngredient = new Ingredient(ingredient.substring(0, quantityIndex), measurement, ingredientTitle.trim().toLowerCase());
+                    if(ingredientDB.size() == 0) {
+                        ingredientService.createIngredient(newIngredient);
+                    } else {
+                        for (Ingredient ing : ingredientDB) {
+                            if (ing.getMeasurement() != null && newIngredient.getMeasurement() != null) {
+                                if (ing.getMeasurement().equals(newIngredient.getMeasurement()) && ing.getQuantity().equals(newIngredient.getQuantity()) && ing.getName().equals(newIngredient.getName())) {
+                                    duplicate = true;
+                                    break;
+                                }
+                            } else {
+                                if (ing.getQuantity().equals(newIngredient.getQuantity()) && ing.getName().equals(newIngredient.getName())) {
+                                    duplicate = true;
+                                    break;
+                                }
+                            }
+                        }
+                        if (!duplicate) {
+                            ingredientService.createIngredient(newIngredient);
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+
+    public void addingIngredientsToRecipe() {
+        List<Recipe> recipes = recipeService.getRecipes("");
+        List<Ingredient> ingredients = ingredientService.getIngredients();
+
+        for (Recipe recipe : recipes) {
+            String recipeIngredientsString = recipe.getIngredients();
+            String[] recipeIngredients = recipeIngredientsString.split(" \\+ ");
+            for (Ingredient ingredient : ingredients) {
+                //Checking for null getMeasurements()
+                String ingString = "";
+                String ingPluralString = "";
+                String ingPluralString2 = "";
+                if (ingredient.getMeasurement() != null) {
+                    //Checking for instances of plural
+                    ingString = ingredient.getQuantity() + " " + ingredient.getMeasurement() + " " + ingredient.getName();
+                    ingPluralString = ingredient.getQuantity() + " " + ingredient.getMeasurement() + "s " + ingredient.getName();
+                    ingPluralString2 = ingredient.getQuantity() + " " + ingredient.getMeasurement() + "es " + ingredient.getName();
+                } else {
+                    //Checking for instances of plural
+                    ingString = ingredient.getQuantity() + " " + ingredient.getName();
+                    ingPluralString = ingredient.getQuantity() + " " + ingredient.getName();
+                    ingPluralString2 = ingredient.getQuantity() + " " + ingredient.getName();
+                }
+                for (String recipeIng : recipeIngredients) {
+                    if (recipeIng.toLowerCase().equals(ingString.toLowerCase()) || recipeIng.toLowerCase().equals(ingPluralString.toLowerCase()) || recipeIng.toLowerCase().equals(ingPluralString2.toLowerCase())) {
+                        IngredientRecipeDto ingredientRecipeDto = new IngredientRecipeDto(recipe.getRecipeId(), ingredient.getIngredientId());
+                        ingredientService.mapIngredientToRecipe(ingredientRecipeDto);
+                    }
+                }
+            }
+        }
+    }
+
 }
